193e068b0fb2de0ebd41026167320de6415ed2a4
XuanSon99 <64201776+XuanSon99@users.noreply.github.com>
2025-03-14 22:49:46 +0700

    Update AffiliateCommission.py

--- AffiliateCommission.py
+++ AffiliateCommission.py
@@ -13,36 +13,28 @@ current_date = datetime.fromtimestamp(current_time / 1000)
 start_time_last_month = int((current_date.replace(day=1, month=2, year=2025, hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)).replace(day=1).timestamp() * 1000)
 end_time_last_month = int((current_date.replace(day=1, month=2, year=2025, hour=0, minute=0, second=0, microsecond=0) - timedelta(seconds=1)).timestamp() * 1000)
 
-params = [
-    {
-        "startTime": int((current_date.replace(hour=0, minute=0, second=0, microsecond=0)).timestamp() * 1000),
-        "endTime": current_time,
-        "pageSize": 100
-    },
-    {
-        "startTime": int((current_date.replace(day=1, hour=0, minute=0, second=0, microsecond=0)).timestamp() * 1000),
-        "endTime": current_time,
-        "pageSize": 100
-    },
-    {
+params = {
         "startTime": start_time_last_month,
         "endTime": end_time_last_month,
         "pageSize": 100
     }
-]
 
-for i, item in enumerate(params):
-    data = rebate.get_affiliate_commission(item)
-    
-    if i == 0:
-        code = "td"
-    if i == 1:
-        code = "cm"
-    if i == 2:
-        code = "lm"
-    
-    print(data)
-    requests.put(f"https://hi.muabanusdt.xyz/api/ref/{code}",{'data': json.dumps(data["data"]["resultList"])})
+
+list = rebate.get_affiliate_commission(params)
+data = list["data"]["resultList"]
+page = list["data"]["totalPage"]
+if page > 1:
+    for i in range(2, page + 1):
+        l = rebate.get_affiliate_commission({
+        "startTime": start_time_last_month,
+        "endTime": end_time_last_month,
+        "pageSize": 100,
+        "page": i
+    })
+        data.extend(l["data"]["resultList"])
+
+        if i == page:
+            a = requests.put(f"https://hi.muabanusdt.xyz/api/ref/lm",{'data': json.dumps(data)})
 
 
 
