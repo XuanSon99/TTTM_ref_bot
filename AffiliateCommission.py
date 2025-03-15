797da0a1389b01496a05759ee9e5a4cb0bca90f0
Your Name <you@example.com>
2025-03-15 20:35:29 +0700

    chawngw

--- AffiliateCommission.py
+++ AffiliateCommission.py
@@ -3,38 +3,63 @@ import requests
 import json
 import time
 from datetime import datetime, timedelta
+from telegram import *
+from telegram.ext import *
+
+domain = "https://hi.muabanusdt.xyz"
+token = "7990958051:AAFdbbvQHda4fKvN6-Dy7Cc8obh_lDnr14A"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Chào mừng bạn đến với <b>Hero Team</b>",
+        parse_mode=constants.ParseMode.HTML,
+    )
+
+async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    username = update.effective_user.username
+    chat_id = update.effective_chat.id
+    print(chat_id)
+     
+app = (
+    ApplicationBuilder().token(token).build()
+)
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.ALL, messageHandler))
+
+def updateData(context: ContextTypes.DEFAULT_TYPE):
+    rebate = mexc_spot_v3.mexc_rebate()
+
+    current_time = int(time.time() * 1000)
+    current_date = datetime.fromtimestamp(current_time / 1000)
+
+    params = {
+            "startTime": int((current_date.replace(day=1, hour=0, minute=0, second=0, microsecond=0)).timestamp() * 1000),
+            "endTime": current_time,
+            "pageSize": 100
+        }
+
+    list = rebate.get_affiliate_commission(params)
+    data = list["data"]["resultList"]
+    page = list["data"]["totalPage"]
+    if page > 1:
+        for i in range(2, page + 1):
+            l = rebate.get_affiliate_commission({
+            "startTime": int((current_date.replace(day=1, hour=0, minute=0, second=0, microsecond=0)).timestamp() * 1000),
+            "endTime": current_time,
+            "pageSize": 100,
+            "page": i
+        })
+            data.extend(l["data"]["resultList"])
+
+            if i == page:
+                requests.put(f"{domain}/api/ref/cm", {'data': json.dumps(data)})
 
 
-rebate = mexc_spot_v3.mexc_rebate()
+job_queue = app.job_queue
 
-current_time = int(time.time() * 1000)
-current_date = datetime.fromtimestamp(current_time / 1000)
+job_minute = job_queue.run_repeating(updateData, interval=60, first=10)
 
-start_time_last_month = int((current_date.replace(day=1, month=2, year=2025, hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)).replace(day=1).timestamp() * 1000)
-end_time_last_month = int((current_date.replace(day=1, month=2, year=2025, hour=0, minute=0, second=0, microsecond=0) - timedelta(seconds=1)).timestamp() * 1000)
-
-params = {
-        "startTime": start_time_last_month,
-        "endTime": end_time_last_month,
-        "pageSize": 100
-    }
-
-
-list = rebate.get_affiliate_commission(params)
-data = list["data"]["resultList"]
-page = list["data"]["totalPage"]
-if page > 1:
-    for i in range(2, page + 1):
-        l = rebate.get_affiliate_commission({
-        "startTime": start_time_last_month,
-        "endTime": end_time_last_month,
-        "pageSize": 100,
-        "page": i
-    })
-        data.extend(l["data"]["resultList"])
-
-        if i == page:
-            a = requests.put(f"https://hi.muabanusdt.xyz/api/ref/lm",{'data': json.dumps(data)})
-
-
+app.run_polling()
 
