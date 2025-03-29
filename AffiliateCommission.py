3fc6b0c1b1df258a2cb62aaf8cd337c61ba3f5c8
XuanSon99 <64201776+XuanSon99@users.noreply.github.com>
2025-03-29 19:42:04 +0700

    Update AffiliateCommission.py

--- AffiliateCommission.py
+++ AffiliateCommission.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from telegram import *
 from telegram.ext import *
 
-domain = "https://hi.muabanusdt.xyz"
+domain = "https://api.taitientrenmang.com"
 token = "7990958051:AAFdbbvQHda4fKvN6-Dy7Cc8obh_lDnr14A"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -19,13 +19,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     username = update.effective_user.username
     chat_id = update.effective_chat.id
-    print(chat_id)
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=chat_id,
-        parse_mode=constants.ParseMode.HTML,
-    )
-     
+
 app = (
     ApplicationBuilder().token(token).build()
 )
@@ -33,38 +27,38 @@ app = (
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.ALL, messageHandler))
 
-# async def updateData(context: ContextTypes.DEFAULT_TYPE):
-#     rebate = mexc_spot_v3.mexc_rebate()
+async def updateData(context: ContextTypes.DEFAULT_TYPE):
+    rebate = mexc_spot_v3.mexc_rebate()
 
-#     current_time = int(time.time() * 1000)
-#     current_date = datetime.fromtimestamp(current_time / 1000)
+    current_time = int(time.time() * 1000)
+    current_date = datetime.fromtimestamp(current_time / 1000)
 
-#     params = {
-#             "startTime": int((current_date.replace(day=1, hour=0, minute=0, second=0, microsecond=0)).timestamp() * 1000),
-#             "endTime": current_time,
-#             "pageSize": 100
-#         }
+    params = {
+            "startTime": int((current_date.replace(day=1, hour=0, minute=0, second=0, microsecond=0)).timestamp() * 1000),
+            "endTime": current_time,
+            "pageSize": 100
+        }
 
-#     list = rebate.get_affiliate_commission(params)
-#     data = list["data"]["resultList"]
-#     page = list["data"]["totalPage"]
-#     if page > 1:
-#         for i in range(2, page + 1):
-#             l = rebate.get_affiliate_commission({
-#             "startTime": int((current_date.replace(day=1, hour=0, minute=0, second=0, microsecond=0)).timestamp() * 1000),
-#             "endTime": current_time,
-#             "pageSize": 100,
-#             "page": i
-#         })
-#             data.extend(l["data"]["resultList"])
+    list = rebate.get_affiliate_commission(params)
+    data = list["data"]["resultList"]
+    page = list["data"]["totalPage"]
+    if page > 1:
+        for i in range(2, page + 1):
+            l = rebate.get_affiliate_commission({
+            "startTime": int((current_date.replace(day=1, hour=0, minute=0, second=0, microsecond=0)).timestamp() * 1000),
+            "endTime": current_time,
+            "pageSize": 100,
+            "page": i
+        })
+            data.extend(l["data"]["resultList"])
 
-#             if i == page:
-#                 requests.put(f"{domain}/api/ref/cm", {'data': json.dumps(data)})
+            if i == page:
+                requests.put(f"{domain}/api/ref/cm", {'data': json.dumps(data)})
 
 
-# job_queue = app.job_queue
+job_queue = app.job_queue
 
-# job_minute = job_queue.run_repeating(updateData, interval=60, first=10)
+job_minute = job_queue.run_repeating(updateData, interval=60, first=10)
 
 app.run_polling()
 
